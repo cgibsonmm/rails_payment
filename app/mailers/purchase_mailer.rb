7605b2e265f071a0e530bfbc0e50a766d9467d0c
Corey Gibson <cgibsonmm@gmail.com>
2017-06-11 17:47:15 -0400

    changed from

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,8 +1,8 @@
 class PurchaseMailer < ActionMailer::Base
   layout 'purchase_mailer'
-  default from: "Learn rails <learnappcode@gmail.com"
+  default from: "learnappcode@gmail.com"
 
-  def purchase_recipt(purchase)
+  def purchase_recipt purchase
     @purchase = purchase
     mail to: purchase.email, subject: 'Thanks for your bussiness!'
   end
